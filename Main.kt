ea7cd51b0a190851bb0f4299a314682005db9741
Johan <johgru95@gmail.com>
2023-03-17 09:55:56 +0100

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,6 +1,10 @@
 fun main() {
+    println("How many numbers are you gonna input?")
     val n = readln().toInt()
+    
+    println("Type your numbers, one after the other.")
     val numberList = mutableListOf<Int>()
     repeat(n) { numberList.add(readln().toInt()) }
+    
     if (numberList == numberList.sorted()) println("YES") else println("NO")
 }
